8bf798d9d7c77305965596724ea5b2b35b3e1b76
Fatih Tuzun <55248838+fatihtuzunn@users.noreply.github.com>
2023-08-17 01:26:59 +0300

    litteraly before multi select comp

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -6,7 +6,7 @@ function Filter({ data, setData }) {
     const [name, setName] = React.useState("");
     const [ageMin, setAgeMin] = React.useState();
     const [ageMax, setAgeMax] = React.useState();
-    const [price, setPrice] = React.useState("All");
+    const [followers, setFollowers] = React.useState("All");
     const [type, setType] = React.useState("All");
     const [gender, setGender] = React.useState("All");
 
@@ -22,14 +22,14 @@ function Filter({ data, setData }) {
         setAgeMin(min);
         setAgeMax(max);
     };
-    const handlepriceChange = (event) => {
-        setPrice(event.target.value);
+    const handleFollowersChange = (event) => {
+        setFollowers(event.target.value);
     };
     const handletypeChange = (event) => {
         setType(event.target.value);
     };
 
-    const priceRe = /\$(.*)\/month/;
+
 
 
 
@@ -58,15 +58,15 @@ function Filter({ data, setData }) {
             setData(filteredData);
         }
 
-        if (price != "All") {
-            const split = price.split("-");
+        if (followers != "All") {
+            const split = followers.split("-");
             const lowerBound = Number(split[0]);
             const upperBound = Number(split[1]);
 
             filteredData = filteredData.filter((el) => {
                 // "$3,050/month"
-                const [_, match] = el.price.match(priceRe);
-                const dataPrice = Number(match.replace(",", ""));
+
+                const dataPrice = el.insta.followers + el.tiktok.followers
 
                 return dataPrice >= lowerBound && dataPrice < upperBound;
             });
@@ -80,32 +80,51 @@ function Filter({ data, setData }) {
         }
 
         setData(filteredData);
-    }, [location, name, type, price, ageMin, ageMax, gender]);
+    }, [location, name, type, followers, ageMin, ageMax, gender]);
 
     return (
         <div>
             <div className='flex flex-col items-center '>
-                <div class="mb-6">
-                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                    <input value={name} onChange={handleNameChange} type="name" id="name" class="bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500  w-56 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+                <div className="mb-5">
+                    <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                    <input value={name} onChange={handleNameChange} type="name" id="name" className="bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500  w-56 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
                 </div>
-                <div>
-                    <label for="gender" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
+                <div className="mb-5">
+                    <label for="gender" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
 
                     <div className="relative inline-flex text-red-700">
                         <svg className="w-4 h-4 absolute top-0 right-0 m-3 pointer-events-none" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
                         </svg>
                         <select onChange={(e) => setGender(e.target.value)} className="w-56 border rounded-lg border-gray-300 text-gray-600 h-10 pl-2 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
-                            <option value="All">Choose a color</option>
+                            <option value="All">All</option>
                             <option value="Male">Male</option>
                             <option value="Female">Female</option>
 
                         </select>
                     </div>
                 </div>
+                <div>
+                    <label for="followers" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Total Followers</label>
+
+                    <div className="relative inline-flex text-red-700">
+                        <svg className="w-4 h-4 absolute top-0 right-0 m-3 pointer-events-none" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                        </svg>
+                        <select onChange={handleFollowersChange} className="w-56 border rounded-lg border-gray-300 text-gray-600 h-10 pl-2 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
+                            <option value="All">All</option>
+                            <option value="0-10000">0 - 10K</option>
+                            <option value="10000-50000">10K - 50K</option>
+                            <option value="50000-200000">50K - 200K</option>
+                            <option value="200000-10000000">200K and above</option>
+
+
+                        </select>
+                    </div>
+                </div>
                 <div className='mt-5 '>
-                    <h3 className='mb-5'>asas</h3>
+                    <label for="age" className=" block mb-4 text-sm font-medium text-gray-900 dark:text-white">Age</label>
+
                     <MultiRangeSlider
                         min={18}
                         max={100}
